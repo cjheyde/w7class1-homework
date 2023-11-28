a282b9c63d35579b9249f4ca9b942774581812f7
cjheyde <carlaheyde@gmail.com>
2023-11-28 22:52:42 +0100

    feat: :construction: Alert message when count is 0 - count cannot be negative

--- src/components/ClickCounterClass.js
+++ src/components/ClickCounterClass.js
@@ -5,13 +5,7 @@ class ClickCounterClass extends Component {
     this.state = {
       count: 0,
     };
-
   }
-  handleClickAdd = () => {
-    this.setState((prevState) => ({
-      count: prevState.count + 1,
-    }));
-  };
 
   handleClickExclude = () => {
     this.setState((prevState) => ({
@@ -19,13 +13,34 @@ class ClickCounterClass extends Component {
     }));
   };
 
+  handleClickAdd = () => {
+    this.setState((prevState) => ({
+      count: prevState.count + 1,
+    }));
+  };
+
+  errorMessage = () => {
+    alert("Count cannot be negative.")
+  }
+
   render() {
     return (
       <div className="blue">
         <h2>Click Counter - Class Component</h2>
         <p>Count: {this.state.count}</p>
         <button onClick={this.handleClickAdd}>Click Me to Add</button>
-        <button onClick={this.handleClickExclude}>Click Me to Exclude</button>
+        <button
+          onClick={
+            this.state.count !== 0 ? (
+              this.handleClickExclude
+            ) :
+            (
+              this.errorMessage
+            )
+          }
+        >
+          Click Me to Exclude
+        </button>
       </div>
     );
   }
--- src/components/ClickCounterFunctional .js
+++ src/components/ClickCounterFunctional .js
@@ -6,8 +6,13 @@ const ClickCounterFunctional = () => {
   const handleClickAdd = () => {
     setCount((prevCount) => prevCount + 1);
   };
+
   const handleClickExclude = () => {
     setCount((prevCount) => prevCount - 1);
+  };
+
+  const errorMessage = () => {
+    alert("Count cannot be negative.");
   };
 
   return (
@@ -15,7 +20,13 @@ const ClickCounterFunctional = () => {
       <h2>Click Counter - Functional Component</h2>
       <p>Count: {count}</p>
       <button onClick={handleClickAdd}>Click Me To Add</button>
-      <button onClick={handleClickExclude}>Click Me To Exclude</button>
+      <button
+        onClick={
+          count !== 0 ? handleClickExclude : errorMessage
+        }
+      >
+        Click Me To Exclude
+      </button>
     </div>
   );
 };
